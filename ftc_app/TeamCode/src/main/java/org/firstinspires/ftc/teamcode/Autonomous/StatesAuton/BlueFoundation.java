d6f933156d9f68a88c9fbf9bb1abc27e767f7add
Wesley Zhuang <wzhuang22@d-e.org>
2020-12-07 14:51:46 -0500

    i think auton

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/StatesAuton/BlueFoundation.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/StatesAuton/BlueFoundation.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.Autonomous.Methods.NewAutonMethods;
 import org.firstinspires.ftc.teamcode.Hardware.Movement;
 
-@Autonomous(name = "Blue Foundation", group = "Autonomous")
+@Autonomous(name = "Just park", group = "Autonomous")
 public class BlueFoundation extends OpMode {
     NewAutonMethods robot = new NewAutonMethods();
 
@@ -44,56 +44,14 @@ public class BlueFoundation extends OpMode {
     public void loop() {
         switch (robot.command) {
             case 0:
-                robot.setTarget(Movement.DOWNRIGHT, 200);
+                robot.setTarget(Movement.FORWARD, 200);
                 break;
 
             case 1:
                 robot.finishDrive();
                 break;
 
-            case 2:
-                robot.newCloseServoAuton();
-                if (robot.runtime.milliseconds() > 450) {
-                    robot.command++;
-                }
-                break;
 
-            case 3:
-                robot.setTarget(Movement.FORWARD, 50);
-                break;
-
-            case 4:
-                robot.finishDrive();
-                break;
-
-            case 5:
-                robot.setTarget(Movement.UPLEFT, 90);
-                break;
-
-            case 6:
-                robot.finishDrive();
-                break;
-
-            case 7:
-                robot.gyroTurn(90);
-                break;
-
-            case 8:
-                robot.setTarget(Movement.BACKWARD , 30);
-                break;
-
-            case 9:
-                robot.newOpenServoAuton();
-                robot.finishDrive();
-                break;
-
-            case 10:
-                robot.setTarget(Movement.FORWARD,120);
-                break;
-
-            case 11:
-                robot.finishDrive();
-                break;
         }
         telemetry.addData("Case:", robot.command);
         telemetry.update();
